15cce616af762de6f71c20476b569e77aeb7ab88
starodubov <sergei.starodubov@simbirsoft.com>
2020-08-17 11:30:13 +0400

    lesson 3

--- src/app/binding-block/binding-block.component.ts
+++ src/app/binding-block/binding-block.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-binding-block',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BindingBlockComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
+  checked: boolean = false
+  range: string
+  handle(e:any) :void {
+    console.log(e.target.value)
+  }
   ngOnInit(): void {
+
   }
 
 }
